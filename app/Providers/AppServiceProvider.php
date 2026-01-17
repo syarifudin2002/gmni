2a68daa612640af3bff2ce20d079c8ebe9063987
syarief <syarifudinahmad457@gmail.com>
2026-01-17 22:55:07 +0800

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+        
+        if ($this->app->environment('production')) {
+        \URL::forceScheme('https');
+    }
     }
 }
--- resources/views/dashboard/admin/datakader/show.blade.php
+++ resources/views/dashboard/admin/datakader/show.blade.php
@@ -2,14 +2,14 @@
 
 @section('container')
     <div class="container">
-         
+
         <div class="container text-center mt-3">
             <img src="{{ asset('storage/' . $kader->image) }}" class="bd-placeholder-img rounded" width="190" height="190"
                 alt="Foto GMNI" style="object-fit: cover;">
             <h1 class="mb-3"> {{ $kader->nama }}</h1>
         </div>
-        <div class="row mt-3">
-            <!-- Kolom Kiri -->
+        {{-- <div class="row mt-3"> --}}
+            {{-- <!-- Kolom Kiri -->
             <div class="col-lg-6 col-md-12">
                 <table class="table table-bordered bg-white">
                     <tr>
@@ -51,9 +51,58 @@
                         <td>{!! $kader->asal_dpk !!}</td>
                     </tr>
                 </table>
-            </div>
-        </div>
+            </div> --}}
+
+            <table class="table table-bordered bg-white kader-table">
+                <tbody>
+                    <tr>
+                        <th>Nama</th>
+                        <td>{{ $kader->nama }}</td>
+                        <th class="d-none d-lg-table-cell">Jurusan</th>
+                        <td class="d-none d-lg-table-cell">{{ $kader->jurusan }}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Alamat</th>
+                        <td>{{ $kader->alamat }}</td>
+                        <th class="d-none d-lg-table-cell">Jenjang Kaderisasi</th>
+                        <td class="d-none d-lg-table-cell">{{ $kader->jenjang_kaderisasi }}</td>
+                    </tr>
+
+                    <tr>
+                        <th>No WhatsApp</th>
+                        <td>{{ $kader->no_whatsapp }}</td>
+                        <th class="d-none d-lg-table-cell">Tahun Kaderisasi</th>
+                        <td class="d-none d-lg-table-cell">{{ $kader->tahun_kaderisasi }}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Kampus</th>
+                        <td>{{ $kader->kampus }}</td>
+                        <th class="d-none d-lg-table-cell">Asal DPK</th>
+                        <td class="d-none d-lg-table-cell">{{ $kader->asal_dpk }}</td>
+                    </tr>
+
+                    <!-- BARIS KHUSUS HP -->
+                    <tr class="d-lg-none">
+                        <th>Jurusan</th>
+                        <td colspan="3">{{ $kader->jurusan }}</td>
+                    </tr>
+                    <tr class="d-lg-none">
+                        <th>Jenjang Kaderisasi</th>
+                        <td colspan="3">{{ $kader->jenjang_kaderisasi }}</td>
+                    </tr>
+                    <tr class="d-lg-none">
+                        <th>Tahun Kaderisasi</th>
+                        <td colspan="3">{{ $kader->tahun_kaderisasi }}</td>
+                    </tr>
+                    <tr class="d-lg-none">
+                        <th>Asal DPK</th>
+                        <td colspan="3">{{ $kader->asal_dpk }}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+        {{-- </div> --}}
     </div>
-
-
 @endsection
